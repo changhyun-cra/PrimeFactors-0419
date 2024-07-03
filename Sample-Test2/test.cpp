fcf89043177de5e3edada8c6f7b74e50d7c44e88
changhyun-cra <hellow6044@gmail.com>
2024-07-03 11:14:20 +0900

    [feature] page37

--- Sample-Test2/test.cpp
+++ Sample-Test2/test.cpp
@@ -7,3 +7,9 @@ TEST(testcasename, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(testcasename, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
